ade172edb2c03d2ed5ca561235003ddabe3affce
unknown <upendara628@gmail.com>
2026-05-13 10:36:27 -0700

    this is the second one

---
+++ variables/sum.java
@@ -0,0 +1,13 @@
+package variables;
+import java.util.*;
+
+public class sum {
+    public static void main(String[] args){
+        System.out.println("sum of first n numbers ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n*(n+1)/2);
+
+    }
+    
+}
