5e437d6c8802a84102a1fe8b19bcc5820757a381
unknown <upendara628@gmail.com>
2026-05-14 12:19:04 -0700

    hello

---
+++ loops/count.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class count{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("your count down ");
+        while(n>0){
+            System.out.println(n);
+            n--;
+        }
+        System.out.print("happy new year ");
+
+    }
+}
