dce2c7005c36e1b5b5b8871048e42a9af6acb7b1
unknown <upendara628@gmail.com>
2026-05-16 11:14:15 -0700

    pattern printing

---
+++ pattern/pyramid.java
@@ -0,0 +1,23 @@
+public class pyramid{
+    public static void main(String[] args){
+        int n = 6;
+        for(int i= 0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<i*2+1;k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for(int i= 0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*(n-1-i)-1;k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ pattern/ultatriangle.java
@@ -0,0 +1,15 @@
+public class ultatriangle{
+    public static void main(String[] args){
+        int n = 7;
+        for(int i=0;i<n;i++){
+            for(int k=0;k<i;k++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<n-i;j++){
+                System.out.print("*");
+            }
+            
+            System.out.println("");
+        }
+    }
+}
