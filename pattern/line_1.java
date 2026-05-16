36c84747b307e51f65cb6f05faaea2b316ffd12f
unknown <upendara628@gmail.com>
2026-05-16 09:17:21 -0700

    pattern

---
+++ pattern/line_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class line_1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your number ");
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println("*");
+            System.out.println(" ");
+        }
+
+    }
+}
